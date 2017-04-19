466f544f7ae99dc3d7bf415d0ef8db90b6f14aec
Wanghao.L <li.wanghao@foxmail.com>
2017-04-19 13:32:59 +0800

    <KWIC-ES> Forget add Input.java

--- es/Input.java
+++ es/Input.java
@@ -1,23 +1,23 @@
 // -*- Java -*-
 /*
  * <copyright>
- * 
+ *
  *  Copyright (c) 2002
  *  Institute for Information Processing and Computer Supported New Media (IICM),
  *  Graz University of Technology, Austria.
- * 
+ *
  * </copyright>
- * 
+ *
  * <file>
- * 
+ *
  *  Name:    Input.java
- * 
+ *
  *  Purpose: Input reads and parses the KWIC input file
- * 
- *  Created: 05 Nov 2002 
- * 
+ *
+ *  Created: 05 Nov 2002
+ *
  *  $Id$
- * 
+ *
  *  Description:
  *    Input reads and parses the KWIC input file
  * </file>
@@ -30,15 +30,15 @@ package kwic.es;
 */
 
 import java.io.BufferedReader;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-import java.io.FileNotFoundException;
-
-import java.util.StringTokenizer;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.StringTokenizer;
 
 /**
- *  An object of the Input class is responsible for reading and parsing the content of 
+ *  An object of the Input class is responsible for reading and parsing the content of
  *  a KWIC input file. The format of the KWIC input file is as follows:
  *  <ul>
  *  <li>Lines are separated by the line separator character(s) (on Unix '\n', on Windows '\r\n')
@@ -94,7 +94,7 @@ public class Input{
 
   public void parse(String file, LineStorageWrapper line_storage){
     try{
-      
+
           // open the file for reading
       BufferedReader reader = new BufferedReader(new FileReader(file));
 
@@ -102,36 +102,36 @@ public class Input{
           // (Note that all line feed chracters are removed by the readLine() method)
       String line = reader.readLine();
       while(line != null){
-        
+
             // parse the line
             // the default delimiter set for StringTokenizer
             // is the set " \t\n\r\f" of characters
             // (Note that the delimiter characters are not
             // themselves treated as tokens)
         StringTokenizer tokenizer = new StringTokenizer(line);
-        
+
             // if this is not an empty line keep the words so that we can add the line
             // to the line storage
         if(tokenizer.countTokens() > 0){
-          
+
               // keep the words
-          ArrayList words = new ArrayList();          
+          ArrayList words = new ArrayList();
           while(tokenizer.hasMoreTokens())
             words.add(tokenizer.nextToken());
-              
+
               // create a String[] representation of the line
           String[] line_rep = new String[words.size()];
           for(int i = 0; i < line_rep.length; i++)
             line_rep[i] = (String) words.get(i);
-            
+
               // add the new line to the storage
           line_storage.addLine(line_rep);
         }
-        
+
             // read next line
         line = reader.readLine();
       }
-      
+
     }catch(FileNotFoundException exc){
 
           // handle the exception if the file could not be found
@@ -140,20 +140,18 @@ public class Input{
       System.exit(1);
 
     }catch(IOException exc){
-      
+
           // handle other system I/O exception
       exc.printStackTrace();
       System.err.println("KWIC Error: Could not read " + file + "file.");
       System.exit(1);
-      
+
     }
   }
+  public String readLine() throws IOException{
 
-//----------------------------------------------------------------------
-/**
- * Inner classes
- *
- */
-//----------------------------------------------------------------------
+      BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+      return reader.readLine();
+}
 
 }
